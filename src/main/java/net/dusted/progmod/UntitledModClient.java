dce492fb9b7db52361c7720e4ac79a1b3ec21dfe
Dustedmc <dustedyt@gmail.com>
2024-01-04 15:39:18 -0300

    initial commit

---
+++ src/main/java/net/dusted/progmod/UntitledModClient.java
@@ -0,0 +1,10 @@
+package net.dusted.progmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UntitledModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
